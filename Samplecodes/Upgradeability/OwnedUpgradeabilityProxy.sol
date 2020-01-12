87b66890055dcf74291d7d94a9df56896426dd64
justCharlz <charles.okafor@sterling.ng>
2020-01-12 05:51:52 +0100

    code sample for upgradeable contracts

---
+++ Samplecodes/Upgradeability/OwnedUpgradeabilityProxy.sol
@@ -0,0 +1,86 @@
+pragma solidity ^0.5.0;
+
+import './UpgradeabilityProxy.sol';
+import './Ownable.sol';
+
+/**
+ * @title OwnedUpgradeabilityProxy
+ * @dev This contract combines an upgradeability proxy with basic authorization control functionalities
+ */
+contract OwnedUpgradeabilityProxy is UpgradeabilityProxy, Ownable {
+  /**
+  * @dev Event to show ownership has been transferred
+  * @param previousOwner representing the address of the previous owner
+  * @param newOwner representing the address of the new owner
+  */
+  event ProxyOwnershipTransferred(address previousOwner, address newOwner);
+
+  // Storage position of the owner of the contract
+  bytes32 private constant proxyOwnerPosition = keccak256("dev500Eth.proxy.owner");
+
+  /**
+  * @dev the constructor sets the original owner of the contract to the sender account.
+  */
+  constructor() public {
+    setUpgradeabilityOwner(owner());
+  }
+
+  /**
+  * @dev Throws if called by any account other than the owner.
+  */
+  modifier onlyProxyOwner() {
+    require(msg.sender == proxyOwner());
+    _;
+  }
+
+  /**
+   * @dev Tells the address of the owner
+   * @return the address of the owner
+   */
+  function proxyOwner() public view returns (address owner) {
+    bytes32 position = proxyOwnerPosition;
+    assembly {
+      owner := sload(position)
+    }
+  }
+
+  /**
+   * @dev Sets the address of the owner
+   */
+  function setUpgradeabilityOwner(address newProxyOwner) internal {
+    bytes32 position = proxyOwnerPosition;
+    assembly {
+      sstore(position, newProxyOwner)
+    }
+  }
+
+  /**
+   * @dev Allows the current owner to transfer control of the contract to a newOwner.
+   * @param newOwner The address to transfer ownership to.
+   */
+  function transferProxyOwnership(address newOwner) public onlyProxyOwner {
+    require(newOwner != address(0));
+    emit ProxyOwnershipTransferred(proxyOwner(), newOwner);
+    setUpgradeabilityOwner(newOwner);
+  }
+
+  /**
+   * @dev Allows the proxy owner to upgrade the current version of the proxy.
+   * @param implementation representing the address of the new implementation to be set.
+   */
+  function upgradeTo(address implementation) public onlyProxyOwner {
+    _upgradeTo(implementation);
+  }
+
+  /**
+   * @dev Allows the proxy owner to upgrade the current version of the proxy and call the new implementation
+   * to initialize whatever is needed through a low level call.
+   * @param implementation representing the address of the new implementation to be set.
+   */
+  function upgradeToAndCall(address implementation) public payable
+    onlyProxyOwner
+    {
+      upgradeTo(implementation);
+      //require(address(this).transfer(msg.value), "yjta");
+  }
+}
