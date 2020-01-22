f30f2b95e663b1c55027d9e06b5d480866612a7a
Sam Ayorinde <ayorindesam2@gmail.com>
2020-01-22 10:02:05 +0100

    Additional functions to New Contract

--- contracts/NewContract.sol
+++ contracts/NewContract.sol
@@ -1,8 +1,11 @@
 pragma solidity >=0.5.0 <0.6.0;
 
 contract Participants {
+	// Event 
+	event NewParticipant(string name, string profession, uint age);
+
 	uint participantsNum = 500;
-	uint 
+	uint activeParticipants = 50 ** participantsNum;
 
 	struct ParticipantData {
 		string name;
@@ -15,6 +18,18 @@ contract Participants {
 
 	// This function create new participants' info
 	function createNewParticipant (string memory _name, string memory _profession, string _hobby, uint age) public {
-		participants.push(ParticipantData(_name, _profession, _hobby, age));
+		// participants.push(ParticipantData(_name, _profession, _hobby, age));
+		uint data = participants.push(ParticipantData(_name, _profession, age)) - 1;
+		emit NewParticipant(data, name, profession, age);
+	}
+
+	function _activeParticipants (string memory _participants) private view returns (uint) {
+		uint participant = uint(keccak256(abi.encodePacked(_participants)));
+		return participant % activeParticipants;
+	}
+
+	function createActiveParticipants (string memory _name, string _memory _profession, string _hobby, uint age) public {
+		uint create = _activeParticipants(_participants);
+		createActiveParticipants(_name, _profession, _hobby, age, create);
 	}
 }
